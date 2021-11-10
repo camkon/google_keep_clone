cb42083df75df47f8fd0128e9d856112483b5022
camkon <80558066+camkon@users.noreply.github.com>
2021-11-10 23:20:35 +0530

    refactor - included side nav to App.js

--- src/src/App.js
+++ src/src/App.js
@@ -1,11 +1,13 @@
 import react from 'react';
 import reactDom from 'react-dom';
+import SideNavBar from './components/navbar_side/side_nav';
 import Nav_structure from './components/navbar_top/nav_structure';
 
 function App() {
   return(
     <div>
       <Nav_structure />
+      <SideNavBar />
     </div>
   )
 }
