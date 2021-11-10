5494b2d1396896cecd283f805dbfc0fc4a9e366d
camkon <80558066+camkon@users.noreply.github.com>
2021-11-10 11:20:17 +0530

    initial commit
    File structure

---
+++ src/src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import reactDom from 'react-dom';
+import Nav_structure from './components/navbar_top/nav_structure';
+
+function App() {
+  return(
+    <div>
+      <Nav_structure />
+    </div>
+  )
+}
+
+export default App;
