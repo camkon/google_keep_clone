f78c1adc322e6920d1f370d23bd67c6e36c000a6
camkon <80558066+camkon@users.noreply.github.com>
2021-12-09 23:33:54 +0530

    refactor - style import

--- src/src/App.js
+++ src/src/App.js
@@ -4,6 +4,9 @@ import reactDom from 'react-dom';
 import SideNavBar from './components/navbar_side/side_nav';
 import Nav_structure from './components/navbar_top/nav_structure';
 
+// style
+import './components/navbar_side/side_nav.css';
+import './components/navbar_top/nav_structure.css';
 
 function App() {
   return(
