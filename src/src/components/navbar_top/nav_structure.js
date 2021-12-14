cdcc48429fa091a9048e80e6ec6ab62ee7dd51d5
camkon <80558066+camkon@users.noreply.github.com>
2021-12-14 13:15:32 +0530

    feat - completed navigation animations
    menu button animations completed.
    
    side navigation bar animations completed.
    
    change style on select implemented.

--- src/src/components/navbar_top/nav_structure.js
+++ src/src/components/navbar_top/nav_structure.js
@@ -25,6 +25,7 @@ import archiveIcon from './../../images/iconsBlack/archive.svg'
 import deleteIcon from './../../images/iconsBlack/delete.svg'
 
 
+
 class Nav_structure extends react.Component {
 
   constructor(props) {
@@ -32,20 +33,32 @@ class Nav_structure extends react.Component {
     this.state = {
       sideNavState: 'side-nav-shrink',
       animeClass: 'side-icon-label labels-hide nodisp',
-      sideIconState: 'icon-back-box',
+      sideIconState: 'icon-back',
+      searchBar: 'search-bar-state',
     }
   }
 
-  //when clicked on menubutton on top-nav-bar, it checks for style class's state and change accordingly 
-  stateCheck = () => {
-    let x = this.state.sideNavState == 'side-nav-stretch' ? 'side-nav-shrink' : 'side-nav-stretch';
-    let y = this.state.animeClass == 'side-icon-label labels-show' ? 'side-icon-label labels-hide nodisp' : 'side-icon-label labels-show';
-    let z = this.state.sideIconState == 'icon-back-round' ? 'icon-back-box' : 'icon-back-round';
+
+
+  searchBarState = () => {
+    let s = this.state.searchBar == '' ? 'search-bar-state' : '';
     this.setState({
-      sideNavState: x,
-      animeClass: y,
-      sideIconState: z,
+      searchBar: s,
     })
+  }
+
+
+
+
+
+
+
+
+
+
+  //when clicked on menubutton on top-nav-bar, it checks for style class's state and change accordingly 
+  stateCheck = (e) => {
+    const x = this.state.sideNavState == 'side-nav-shrink' ? this.enterSideNav() : this.exitSideNav();
   }
 
   //function to change the style class to STRETCH side nav when pointer exits the perimeter
@@ -77,18 +90,55 @@ class Nav_structure extends react.Component {
   }
 
 
-  iconSelect = (e) => {
-    if(e.target.getAttribute('class') == 'side-icons') {
-      let a = e.target.getAttribute('data-name');
-      let b = document.querySelector(`[data-name=${a}]`)
-      console.log(b.children[1]);
-      b.style.backgroundColor = 'rgb(255, 197, 122)'
-      b.children[1].style.backgroundColor = 'rgb(255, 197, 122)'
+
+  
+
+  
+
+
+  iconSelectCheck = (e) => {
+    let d = e.target.getAttribute('class')
+
+    if(d == 'side-icons') {
+      if(e.target.getAttribute('id') != null) console.log('this is already selected');
+      else {
+        let selectIcon = () => {e.target.setAttribute('id','icon-selected')}
+        this.checkForOtherIconSelection(selectIcon)
+      }
+    }
+
+    else if(d == 'img') {
+      if(e.target.parentNode.getAttribute('id') != null) console.log('this is already selected');
+      else {
+        let selectIcon = () => {e.target.parentNode.setAttribute('id','icon-selected')}
+        this.checkForOtherIconSelection(selectIcon)
+      }
+    }
+
+    else {
+      console.log('side-label selected');
+      if(e.target.parentNode.getAttribute('id') != null) console.log('this is already selected');
+      else {
+        let selectIcon = () => {e.target.parentNode.setAttribute('id','icon-selected')}
+        this.checkForOtherIconSelection(selectIcon)
+      }
     }
   }
 
-
-
+  checkForOtherIconSelection = (x) => {
+    let a = document.querySelectorAll('.side-icons')
+    for(const i of a) {
+      console.log('times : ' + i);
+      let b = i.getAttribute('data-name')
+      let c = document.querySelector(`[data-name=${b}]`)
+      if(c.getAttribute('id') == 'icon-selected') {
+        // console.log(b);
+        c.removeAttribute('id');
+        x();
+        break;
+      }
+    }
+  }
 
 
 
@@ -108,7 +158,7 @@ class Nav_structure extends react.Component {
           </div>
           
           <div className="top_nav_grids" id="middleNav">
-            <div id="searchBar" className={this.state.searchBar} onClick={this.searchBarChange}>
+            <div className="searchBar" id={this.state.searchBar}>
               <div id="searchIcon"><img src={searchIcon}/></div>
               <input id="searchText" type="text" placeHolder="Search"/>
             </div>
@@ -130,19 +180,19 @@ class Nav_structure extends react.Component {
           id={this.state.sideNavState} 
           onMouseEnter={this.enterSideNav}
           onMouseLeave={this.exitSideNav}
-          onClick={this.iconSelect} 
+          onClick={this.iconSelectCheck} 
           >
 
           <div className="plain-div-behind-side-nav"></div>
 
 
-          <div className='side-icons' data-iconAnime={this.state.sideIconState} id data-name="bulbIcon">
+          <div className='side-icons' data-iconAnime={this.state.sideIconState} id='icon-selected' data-name="bulbIcon">
             <img className='img' src={bulbIcon}/>
             <div className={this.state.animeClass}>Notes</div>
           </div>
 
           <div className='side-icons' data-iconAnime={this.state.sideIconState} data-name="reminderIcon">
-            <img className='img' src={reminderIcon}/>
+            <img className='img' data-name="reminderIcon" src={reminderIcon}/>
             <div className={this.state.animeClass}>Reminder</div>
           </div>
           
