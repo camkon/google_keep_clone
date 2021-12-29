532f95ca511e8f093546344dbbd1a53a9e69dadd
camkon <80558066+camkon@users.noreply.github.com>
2021-12-29 20:29:57 +0530

    feat - added note adding functionality
    used react hooks to add the taken notes to seperate page section.
    
    Added a function that modifies the state of note taking component

--- src/components/Notes/notes.js
+++ src/components/Notes/notes.js
@@ -1,38 +1,63 @@
-import { continueStatement } from '@babel/types';
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import './notes.css';
+import Pages from './page';
 
 const Notes = () => {
 
   const [note_state, setNoteState] = useState('2.9rem');
   const [note_title, setNoteTitle] = useState('');
+  const [note_text, setNoteText] = useState('');
+  const [page_set, setPageSet] = useState([]);
 
 
 
   //functions
   const changeNoteHeight = () => { setNoteState('8.5rem'); }
+
+  //1
+  const takeNoteTitle = (e) => {
+    setNoteTitle(e.target.value);
+  }
+
+  //2
+  const takeNoteText = e => {
+    setNoteText(e.target.value)
+  }
+
+  // 3  
+  // useEffect(() => {
+  //   window.addEventListener('click', (e) => {
+  //     let x = e.target.getAttribute('id')
+  //     if((x != 'note') && (x != 'note-title' && x != 'note-text')) {
+  //       setNoteState('2.9rem');
+  //       makeAPage();
+  //     }
+  //   })
+  // })
+
+
   useEffect(() => {
     document.querySelector(`[data-container="main-screen-container"]`).addEventListener('click', (e) => {
       let x = e.target.getAttribute('id')
-      if(x != 'note-title') {
-        setNoteState('2.9rem')
+      if(x === 'main-screen' || x === 'note-taker' || x === 'note-show') {
+        setNoteState('2.9rem');
       }
     })
   },[])
 
-  const takeNoteTitle = (e) => {
-    setNoteTitle(e.target.value);
-    console.log(note_title);
+
+  const makeAPage = () => {
+    let a = new Array();
+    a.push([note_title,note_text]);
+    setPageSet(a[0]);
   }
 
 
-
   return (
-    <div>
-      <NoteTakeContainer className="note-take-container">
-
-        <Note className="note" noteHeight={note_state} onClick={changeNoteHeight}>
+    <NotesCont className='notes-cont'>
+      <NoteTakeContainer className="note-take-container" id="note-taker">
+        <Note className="note" id="note" noteHeight={note_state} onClick={changeNoteHeight}>
           {
             note_state === '8.5rem' && <NoteTitle 
               className="note-title" 
@@ -42,13 +67,66 @@ const Notes = () => {
               onChange={takeNoteTitle}
               />
           }
-          <NoteTake className="note-take" type="text" placeholder="Take a note..."/>
+          <NoteTake 
+            className="note-take" 
+            type="text" 
+            id="note-text"
+            placeholder="Take a note..."
+            value={note_text}
+            onChange={takeNoteText}
+            />
 
+          {
+            note_state === '8.5rem' && <NoteAdd
+              id="note-add"
+              className="note-add"
+              onClick={makeAPage}>Add
+            </NoteAdd>
+          }
         </Note>
-
       </NoteTakeContainer>
 
-    </div>
+      <NoteShowContainer id="note-show" contHeight={note_state} className="note-show-container">
+      {
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }
+      {/* {
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      }{
+        page_set != null && <Pages title={page_set[0]} text={page_set[1]}/>
+      } */}
+      </NoteShowContainer>
+    </NotesCont>
   )
 }
 
@@ -56,6 +134,9 @@ export default Notes
 
 
 //styled components
+
+const NotesCont = styled.div`
+`
 
 const NoteTakeContainer = styled.div`
   // display: flex;
@@ -90,5 +171,12 @@ const NoteTake = styled.input`
   // border: none;
   // border-radius: 0.5rem;
   // background-color: ;
+`
+const NoteAdd = styled.button``
+
+const NoteShowContainer = styled.div`
+  position: absolute;
+  top: ${props => props.contHeight === '2.9rem' ? '5.9rem' : '11.5rem'};
+  height: ${props => props.contHeight == '2.9rem' ? 'calc(100% - 5.9rem)' : 'calc(100% - 11.5rem)'}
 `
 
