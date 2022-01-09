22313f890c3411905e37122db3f267e7f4a2b14d
camkon <80558066+camkon@users.noreply.github.com>
2022-01-09 14:09:50 +0530

    refactor - implemented smooth note taking

--- src/components/Notes/page.js
+++ src/components/Notes/page.js
@@ -1,29 +1,21 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import styled from 'styled-components';
-
-const allNotesArr = [];
 
 const Pages = ({frag}) => {
 
-
-
-  useEffect(() => {
-    console.log('1')
-    allNotesArr.push(frag);
-  },[frag])
-
-  return (true && allNotesArr.map((i) => {
-    if(i[0] != undefined || i[1] != undefined) {
+  return (true && frag.map((i) => {
+    if((i.title === undefined || i.title === '') && (i.text === undefined || i.text === '')) {
+      return 0;
+    }
+    else 
+    {
       return(
         <Page>
-          <Title>{i[0]}</Title>
-          <Text>{i[1]}</Text>
+          <Title>{i.title}</Title>
+          <Text>{i.text}</Text>
         </Page>
         )
       }
-    else {
-      console.log('4') //2
-    }
   }))
 }
 
@@ -31,29 +23,39 @@ export default Pages;
 
 const Page = styled.div`
   min-width: 15rem;
-  max-width: 20rem;
-  height: 10rem;
-  padding: 0rem 0.7rem;
+  max-width: 25rem;
+  display: flex;
+  align-content: flex-start;
+  flex-flow: row wrap;
+  max-height: 25rem;
+  min-height: 10rem;
+  padding: 0.7rem;
   margin: 1rem;
-  border: 1px solid black;
+  border: 1px solid rgb(200,200,200);
   border-radius: 0.5rem;
   box-sizing: border-box;
   overflow: hidden;
-  color: black;
+  color: rgb(40,40,40);
 `
 
 const Title = styled.p`
   min-height: 1.5rem;
+  max-height: 3.5rem;
+  display: flex;
+  width: 100%;
+  margin: 0;
   font-size: 1rem;
-  font-family: roboto;
+  font-family: monospace;
   letter-spacing: 2px;
   font-weight: 600;
 `
 
 const Text = styled.p`
+  margin: 0.5rem 0 0 0;
+  display:flex;
   word-wrap: break-word;
-  font-size: 1rem;
-  font-family: roboto;
-  letter-spacing: 0.3px;
+  font-size: 1.15rem;
+  font-family: monospace;
+  letter-spacing: 0.25px;
   overflow-y: hidden;
 ` 
